80bebb507c47cd7bd5344504478a43ac8b784bb4
Delwar Hussain <Dilwar1998@hotmail.co.uk>
2021-11-24 19:15:34 +0000

    sub totol, totol bill and split bill

--- restaurant.py
+++ restaurant.py
@@ -3,50 +3,38 @@ class Table:
         self.num_people = num_people
         self.bill =[]
 
+
     def order(self, item, price, qty = 1):
-        for x in self.bill:
-            if x["item"] == item and x["price"] == price:
-                print(x["item"])
-                x["quantity"] = x.get("quantity")+qty
-                print(x["quantity"])
+        for menu_item in self.bill:
+            if menu_item["item"] == item and menu_item["price"] == price:
+                menu_item["quantity"] = menu_item.get("quantity") + qty
                 return None
         self.bill.append({"item": item, "price": price, "quantity": qty})
 
     def remove(self, item, price, qty = 1):
-        for x in self.bill:
-            if x["item"] == item and x["price"] == price:
-                print(x["item"])
-                x["quantity"] = x.get("quantity") - qty
-                if x["quantity"] <=0:
-                    self.bill.remove(x)
+        for menu_item in self.bill:
+            if menu_item["item"] == item and menu_item["price"] == price:
+                menu_item["quantity"] = menu_item.get("quantity") - qty
+                if menu_item["quantity"] <=0:
+                    self.bill.remove(menu_item)
 
 
     def get_subtotal(self):
-        '''
-        - A `get_subtotal` method that returns the total cost for the table based on the prices and quantities in the bill.
+        total = 0.00
+        for item in self.bill:
+            total += item["price"] * item["quantity"]
+        return round(total,2)
 
-        :return:
-        '''
-        pass
-
-    def get_total(self):
-        '''
-        - A `get_total` method that accepts a service charge percentage in the form of a decimal.  If no service charge percentage is provided,
-         it should default to 10% (i.e. `0.10`).  This method should return a dictionary with the following
-         keys: `Sub Total`, `Service Charge`, `Total`.  The values should be string representations of the corresponding prices
-          in British pounds and pence.  e.g. `{"Sub Total": "£120.00", "Service Charge": "£12.00", "Total": "£132.00"}`
-
-        :return:
-        '''
-        pass
+    def get_total(self, service_charge_percentage:float):
+        subtotal = self.get_subtotal()
+        ser_charge = round(subtotal * service_charge_percentage,2)
+        total = subtotal + ser_charge
+        total_bill ={"Sub Total": f"£{subtotal}","Service Charge": f"£{ser_charge}","Total":f"£{total}"}
+        return total_bill
 
     def split_bill(self):
-        '''
-        - A `split_bill` method, which returns the the subtotal cost of the bill divided by the number of diners as a float rounded up
-        to the nearest penny.
-
-        :return:
-        '''
-        pass
+        subtotal = self.get_subtotal()
+        per_person = round(subtotal/self.num_people,2)
+        return per_person
 
 
