b777c681835a7f69f0895c8b221fa7495cb47b1e
AM <83598489+a-miah@users.noreply.github.com>
2021-11-24 22:36:26 +0000

    order and remove method

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,27 @@
 class Table:
-    pass
+    def __init__(self, number_of_diners):
+        self.number_of_diners = number_of_diners
+        self.bill = []
+
+    def order(self, item, price, quantity=1):
+        self.item = item
+        self.price = price
+        for menu_item in self.bill:
+            if menu_item["item"] == item and menu_item["price"] == price:
+                menu_item["quantity"] += quantity
+                return
+
+        self.bill.append({"item": item, "price": price, "quantity": quantity})
+
+        print("Item has been added.")
+
+
+    def remove(self, item, price, quantity=1):
+        for menu_item in self.bill:
+            if menu_item["item"] == item and menu_item["price"] == price:
+                menu_item["quantity"] -= quantity
+                if menu_item["quantity"] <= 0:
+                    self.bill.remove(menu_item)
+
+        print("Item has been removed.")
+
