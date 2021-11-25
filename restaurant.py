4579af6dd34ec774d5fa85c0d6ed41e3d082e6d4
AM <83598489+a-miah@users.noreply.github.com>
2021-11-25 00:16:11 +0000

    6/7 tests passed and comments added

--- restaurant.py
+++ restaurant.py
@@ -3,6 +3,8 @@ class Table:
         self.number_of_diners = number_of_diners
         self.bill = []
 
+    # order list created with item, price and quantity
+
     def order(self, item, price, quantity=1):
         self.item = item
         self.price = price
@@ -15,6 +17,7 @@ class Table:
 
         print("Item has been added.")
 
+    # order list updated if item removed
 
     def remove(self, item, price, quantity=1):
         for menu_item in self.bill:
@@ -25,27 +28,31 @@ class Table:
 
         print("Item has been removed.")
 
+    # works out the total price of the table
 
     def get_subtotal(self):
         menu_total = 0
         for menu_item in self.bill:
-            total += menu_item["price"] * menu_item["quantity"]
-            return round(menu_total, 2)
+            menu_total += menu_item["price"] * menu_item["quantity"]
 
+        return round(menu_total, 2)
+
+    # total bill after the surcharge included
 
     def get_total(self, service_charge=0.1):
         subtotal = self.get_subtotal()
         total_service_charge = round((subtotal * service_charge), 2)
         total = subtotal + total_service_charge
         total_bill = {
-            "Sub Total": subtotal,
-            "Service Charge": total_service_charge,
-            "Total": total
+            "Sub Total": f"£{subtotal}",
+            "Service Charge": f"£{total_service_charge}",
+            "Total": f"£{total}"
         }
         return total_bill
 
+    # total bill split in equal amount for diners
 
-    def split_bill(self, num_of_diners):
+    def split_bill(self):
         subtotal = self.get_subtotal()
-        each_diner = subtotal / num_of_diners
+        each_diner = subtotal / self.number_of_diners
         return each_diner
