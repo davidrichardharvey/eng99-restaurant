662f9ea133e50868c043a5e104a0e3587176656a
RajsJohal <rajsjohal97@hotmail.com>
2021-11-25 09:51:24 +0000

    Updated order to append a dictionary to a list instead of using multiple dictioanries

--- restaurant.py
+++ restaurant.py
@@ -12,25 +12,25 @@ class Table:
             "Total": 0.00
         }
 
-    def order(self, item: str, price: float, quantity=1):#Create a bill
-        if self.bill.get("item") == item and self.bill.get("price") == price:
-            return False
-        else:
-            self.bill = {
-                "item": item,
-                "price": price,
-                "quantity": quantity
-            }
-            return self.bill
+    def order(self, item: str, price: float, quantity=1): # Create a bill
+        bill = []
+        self.bill_copy = self.bill = {
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        }
+        self.bill_copy = self.bill.copy()
+        bill.append(self.bill_copy)
+        return bill
 
     def remove(self, item, price, quantity): #modify bill to decrease quantity
-        if self.bill.get("item") == item and self.bill.get("price") == price and self.bill.get("quantity") > 0:
-            old_quantity = self.bill.get("quantity")
-            new_quantity = old_quantity - quantity
-            self.bill.update({"quantity": new_quantity})
-            return self.bill
-        else:
-            return False
+        #if self.bill.get("item") == item and self.bill.get("price") == price and self.bill.get("quantity") > 0:
+        old_quantity = self.bill.get("quantity")
+        new_quantity = old_quantity - quantity
+        self.bill.update({"quantity": new_quantity})
+        return self.bill
+        #else:
+         #   return False
 
     def get_subtotal(self): # total the price of a table
         price_of_item = self.bill.get("price")
@@ -43,18 +43,18 @@ class Table:
 
     def get_total(self, service_charge = 0.10): #subtotal plus service charge
         sub_total = self.get_subtotal()
-        print(sub_total)
         service_amount_charged = sub_total * service_charge
         total = sub_total + service_amount_charged
-        self.total_bill = {"Sub Total": sub_total,
-                           "Service Charge": service_charge,
-                           "Total": total}
+        self.total_bill = {"Sub Total": f"£{sub_total:.2f}",
+                           "Service Charge": f"£{service_amount_charged:.2f}",
+                           "Total": f"{total:.2f}"}
         return self.total_bill
 
     #def split_bill(self): #divide the bill between all the people at the table
 
 table02 = Table(2)
 print(table02.order("Food", 10.00, 3))
+print(table02.order("Food", 10.00, 5))
 print(table02.remove("Food", 10.00, 1))
 print(table02.get_subtotal())
 print(table02.get_total(0.15))
