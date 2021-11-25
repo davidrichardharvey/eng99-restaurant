046ccf2ba77d70f24f3e0f48ae53fc75ab2fd159
Ivan Kalashnikov <ivan@kalashnikov.uk>
2021-11-25 08:12:57 +0000

    Working order with two tests passing

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,25 @@
+
 class Table:
-    pass
+    def __init__(self, num):
+        self.bill = []
+        self.num = num
+
+    def order(self, item, price, amount=1):
+        self.item = item
+        self.price = price
+        for items in self.bill:
+            if items["item"] == item and items["price"] == price:
+                item["quantity"] += amount
+        self.bill.append({"item": item, "price": price, "quantity": amount})
+
+    def remove(self, item, price, amount=1):
+        del self.bill[item]
+
+    def get_subtotal(self):
+        pass
+
+    def get_total(self, param):
+        pass
+
+    def split_bill(self):
+        pass
